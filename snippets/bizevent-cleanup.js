ac2e85881b665d21ff15edc45d3dc3b2919b1055
Adam Gardner <adam.gardner@dynatrace.com>
2025-09-19 13:48:54 +1000

    Deployed cfea64a with MkDocs version: 1.6.1

---
+++ snippets/bizevent-cleanup.js
@@ -0,0 +1,6 @@
+<script>
+var CURRENT_PAGE = "4. cleanup";
+document.addEventListener('DOMContentLoaded', function() {
+  dynatrace.sendBizEvent('page_load', {"page": CURRENT_PAGE});
+});
+</script>
